2761a0032454efb01997c03ef275025d7e800527
An3sha <anishasingh439@gmail.com>
2025-01-07 16:46:01 +0530

    added review

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css"
 import Editor from "./components/Editor"
+import Review from "./components/Review"
 // A simple code reviewer app
 // we will have two columns layout
 // where we canw rite code
@@ -9,6 +10,7 @@ function App() {
   return (
   <div className="flex h-full w-full items-center justify-center overflow-hidden">
       <Editor />
+      <Review />
        
    
     </div>
